af48bb9e54b7876e80f4dc91cd9c4bb7e3809582
wangsijie <sijiewg@gmail.com>
2026-07-29 04:23:48 +0000

    Revert "Fix cursor flicker across session rows"
    This reverts commit 93a0845f59f78291ade87b1663f71f410db6d82a.

--- web/src/catalog-refresh.ts
+++
@@ -1,8 +0,0 @@
-import type { Route } from './router';
-
-export function enteredNewSessionPage(
-  previous: Route['name'],
-  current: Route['name']
-): boolean {
-  return current === 'list' && previous !== 'list';
-}
